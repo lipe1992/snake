e31c4043decad3b59ec1154c1a4b9d53d4c43037
lipe1992 <46667309+lipe1992@users.noreply.github.com>
2020-05-09 18:07:54 -0500

    Add files via upload

---
+++ SNAKE/js/init.js
@@ -0,0 +1,6 @@
+var game;
+function initSnake(level){
+    console.log(level);
+    createCanvas(600,600)
+    snake = new Snake();
+}
---
+++ SNAKE/js/sketch.js
@@ -0,0 +1,41 @@
+
+var snake;
+var scl = 20;
+var food;
+var initGame = function setup(){
+    createCanvas(600,600)
+    snake = new Snake();
+    frameRate(10);
+    pickLocation(); 
+}
+var pickLocation = function pickLocation(){
+    var cols = floor(width/scl);
+    var rows = floor(width/scl);
+    food = createVector(floor(random(cols)),floor(random(rows)));
+    food.mult(scl);
+}
+
+function draw(){
+    background(51)
+    snake.update();
+    snake.show();
+
+    if(snake.eat(food)){
+        pickLocation();
+    }
+    fill(255,0,100);
+    rect(food.x,food.y,scl,scl);
+}
+
+var keyPressed = function keyPressed(){
+    if(keyCode === UP_ARROW){
+        snake.dir(0,-1);
+    }else if(keyCode === DOWN_ARROW){
+        snake.dir(0,1);
+    }else if(keyCode === RIGHT_ARROW){
+        snake.dir(1,0);
+    }else if(keyCode === LEFT_ARROW){
+        snake.dir(-1,0);
+    }
+}
+
